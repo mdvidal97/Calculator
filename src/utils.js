df6dbc073a552474fb96cf365e72b6d3e81c3f51
Daniela <d00452350@utahtech.edu>
2023-11-18 16:21:25 -0700

    Updating

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 const isEven = (number) => number % 2 === 0;
 
-const isPositive = (number) => number >= 0;
+const isPositive = (number) => number > 0; // Update the condition here
 
 module.exports = {
   isEven,
